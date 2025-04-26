3526d9b39d1c0dfd51f502219fe635d212891016
sathish0010 <153985900+sathish0010@users.noreply.github.com>
2025-04-26 23:09:10 +0530

    Add files via upload

---
+++ Nested Loop/nested for_ex_another.java
@@ -0,0 +1,11 @@
+//18_ex
+
+class demo{
+    public static void main(String args[]){
+        for(int i=1;i<=3;i=i+1){
+            for(int j=1;j<=2;j=j+1){
+                System.out.println("karthi");
+            }
+        }
+    }
+}
---
+++ Nested Loop/nested_for_ex.java.java
@@ -0,0 +1,11 @@
+//ex_method
+
+class demo{
+    public static void main(String args[]){
+        for(int teacher=1;teacher<=3;teacher=teacher+1){
+            for(int j=1;j<=5;j=j+1){
+                System.out.println("karthi");
+            }
+        }
+    }
+}
