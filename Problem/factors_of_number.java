8570346e487f8dcaf6ccddd3d5ff2ad6221ddcb1
sathish0010 <153985900+sathish0010@users.noreply.github.com>
2025-05-06 23:36:30 +0530

    Add files via upload

---
+++ Problem/factors_of_number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class demo{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=scan.nextInt();
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
